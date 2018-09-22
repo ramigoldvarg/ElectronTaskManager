90cf806ee5ffb77d27ffca999c32ac4e6a13f0fc
Rami Goldvarg <ramigoldvarg7@gmail.com>
2018-09-22 18:09:11 +0300

    feat(option-menu): option menu now floating

--- src/components/OptionMenu.vue
+++ src/components/OptionMenu.vue
@@ -1,9 +1,6 @@
 <template>
     <span class="menu">
-        <button class="dots" @click="menuSwitch">
-            .
-            .
-            .
+        <button class="dots fa fa-ellipsis-v" @click="menuSwitch">
         </button>
         <div class="options" v-if="pressed">
             <span v-for="option in options" @click="option.action">
@@ -35,12 +32,18 @@
         background-color: #eee
     }
     .dots {
-        font-weight: bold;
+        opacity: 0.75;
         background-color: rgba(0,0,0,0); 
         border-color: rgba(0,0,0,0);
     }
+    .options {
+        width: 3em;
+        position: absolute;
+        z-index: 100;
+        box-shadow: 0 0 4px rgba(0,0,0,0.2);
+        background-color: white
+    }
     .options>span {
-        display: block;
-        z-index: 1
+        width: 100%
     }
 </style>
